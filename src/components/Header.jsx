9f29ac31660cd008883ea48630fd304588a14dfe
APWasilenko <APWasilenko@gmail.com>
2022-10-06 21:23:04 +0700

    Добавил данные в шапку

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,35 +4,30 @@ import '../css/Header.css';
 const Header = () => {
 
   return (
-    <div className='header'>
-      <div className='header-block'>
-        <div className='header-inline'>
-          Начало смены
-        </div>
-        <div className='header-inline'>
-          :
-        </div>
-        <div className='header-inline'>
-          07:45 23.08.2022г.
-        </div>
-        <div className='header-inline'>
-          -
-        </div>
-      </div>
-      <div className='header-block'>
-        <div className='header-inline'>
-          Конец смены
-        </div>
-        <div className='header-inline'>
-          :
-        </div>
-        <div className='header-inline'>
-          17:45 23.08.2022г.
-        </div>
-        <div className='header-inline'>
-          -
-        </div>
-      </div>
+    <div class="header">
+      <div id="left" class="md1"></div>
+      <div id="center" class="md2">Дата время</div>
+      <div id="center" class="md3">Одометр</div>
+      <div id="center" class="md4">Баланс</div>
+      <div id="center" class="md5">Автомобиль</div>  
+      <div class="mydiv1"></div>
+      <div id="left" class="md1">Начало</div>
+      <div id="right" class="md2">7:45 06.09.2022</div>
+      <div id="right" class="md3">602 457 км</div>
+      <div id="right" class="md4">10 789 ₽</div>
+      <div id="center" class="md5">889 ₽</div>
+      <div class="mydiv1"></div>
+      <div id="left" class="md1">Конец</div>
+      <div id="right" class="md2">16:45 06.09.2022</div>
+      <div id="right" class="md3">602 777 км</div>
+      <div id="right" class="md4">10 389 ₽</div>
+      <div id="center" class="md5">Доход</div>
+      <div class="mydiv1"></div>
+      <div id="left" class="md1">ИТОГО</div>
+      <div id="center" class="md2">10:05</div>
+      <div id="right" class="md3">320 км</div>
+      <div id="right" class="md4">312 ₽</div>
+      <div id="center" class="md5">1 889 ₽</div>
     </div>
   )
 }
