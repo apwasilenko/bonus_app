088acff3152b39ed68241150cc71e7d6f932de0a
Антон Василенко <apwasilenko@gmail.com>
2022-10-10 07:26:53 +0700

    Изменена ширина

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../css/Header.css';
 
+
 const Header = () => {
+
+  const [mywidth, setwidth] = useState('');
+  const [myheight, setheigh] = useState('');
+
+  function setStates() {
+    setwidth(window.innerWidth);
+    setheigh(window.innerHeight);
+  }
 
   return (
     <div class="header">
@@ -9,7 +18,7 @@ const Header = () => {
       <div id="center" class="md2">Дата время</div>
       <div id="center" class="md3">Одометр</div>
       <div id="center" class="md4">Баланс</div>
-      <div id="center" class="md5">Автомобиль</div>  
+      <div id="center" class="md5">Автомобиль</div>
       <div class="mydiv1"></div>
       <div id="left" class="md1">Начало</div>
       <div id="right" class="md2">7:45 06.09.2022</div>
@@ -28,7 +37,12 @@ const Header = () => {
       <div id="right" class="md3">320 км</div>
       <div id="right" class="md4">312 ₽</div>
       <div id="center" class="md5">1 889 ₽</div>
+      <div class="mydiv1"></div>
+      <div>Ширина окна - {mywidth}</div>
+      <div>Высота окна - {myheight}</div>
+      <button onClick={setStates}>Вычислить</button>
     </div>
+
   )
 }
 
