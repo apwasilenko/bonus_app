9c6b80d505e517bdcc580bb7144ba8d1408da8ab
APWasilenko <APWasilenko@gmail.com>
2022-10-13 19:04:48 +0700

    19:00 - 13-10-22

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import PostList from './components/PostList';
 import CanvasDraw from './components/CanvasDraw';
 import './css/App.css';
 
+
+
 function App() {
-  const [posts] = useState([
+    const [posts] = useState([
     { num: 1, time: '12:23', date: '22.09.2022', distance: '1.2', vremy: '3', summa: '90', comis: '9', },
     { num: 11, time: '12:33', date: '22.09.2022', distance: '12.2', vremy: '12', summa: '200', comis: '30', },
     { num: 111, time: '12:42', date: '22.09.2022', distance: '124.2', vremy: '134', summa: '2 380', comis: '119', },
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -5,29 +5,29 @@ const Postitem = function (props) {
   console.log(props);
 
   return (
-  <div className='post'>
-    <div className='post__content'>
-      <div className='post_elem'>
+  <div className="post">
+    <div className="post__content">
+      <div className="post_elem">
         <div className="num_zakaz" id="right">{props.post.num}.</div>
       </div>
-      <div className='post_elem'>
+      <div className="post_elem">
         <div className="time_zakaz" id="right">{props.post.time}</div>
         <div className="date_zakaz" id="right">{props.post.date}</div>
       </div>
-      <div className='post_elem'>
+      <div className="post_elem">
         <div className="dist_zakaz" id="right">{props.post.distance} км.</div>
       </div>
-      <div className='post_elem'>
+      <div className="post_elem">
         <div className="vremy_zakaz" id="right">{props.post.vremy} мин.</div>
       </div>
-      <div className='post_elem'>
+      <div className="post_elem">
         <div className="summ_zakaz" id="right">{props.post.summa} руб.</div>
       </div>
-      <div className='post_elem'>
+      <div className="post_elem">
         <div className="comis_zakaz" id="right">{props.post.comis} руб.</div>
       </div>
     </div>
-    <div className='post__btns'>
+    <div className="post__btns">
       <button>Изменить</button>
       <br />
       <button>Удалить</button>
