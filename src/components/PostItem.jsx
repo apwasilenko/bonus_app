2723ada3c6f31193e4691c11292950082db4b808
Антон Василенко <apwasilenko@gmail.com>
2022-10-15 21:35:06 +0700

    Поправил размер элементов в списке

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -5,34 +5,34 @@ const Postitem = function (props) {
   console.log(props);
 
   return (
-  <div className="post">
-    <div className="post__content">
-      <div className="post_elem">
-        <div className="num_zakaz" id="right">{props.post.num}.</div>
+    <div className="post">
+      <div className="post__content">
+        <div className="post_elem">
+          <div className="num_zakaz" id="right">{props.post.num}.</div>
+        </div>
+        <div className="post_elem">
+          <div className="time_zakaz" id="center">{props.post.time}</div>
+          <div className="date_zakaz" id="center">{props.post.date}</div>
+        </div>
+        <div className="post_elem">
+          <div className="dist_zakaz" id="right">{props.post.distance}_</div>
+          <div className="ed_izm" id="left">км.</div>
+          <div className="prenos"></div>
+          <div className="vremy_zakaz" id="right">{props.post.vremy}_</div>
+          <div className="ed_izm" id="left">мин.</div>
+        </div>
+        <div className="post_elem">
+          <div className="summ_zakaz" id="right">{props.post.summa} руб.</div>
+
+          <div className="comis_zakaz" id="right">{props.post.comis} руб.</div>
+        </div>
       </div>
-      <div className="post_elem">
-        <div className="time_zakaz" id="right">{props.post.time}</div>
-        <div className="date_zakaz" id="right">{props.post.date}</div>
-      </div>
-      <div className="post_elem">
-        <div className="dist_zakaz" id="right">{props.post.distance} км.</div>
-      </div>
-      <div className="post_elem">
-        <div className="vremy_zakaz" id="right">{props.post.vremy} мин.</div>
-      </div>
-      <div className="post_elem">
-        <div className="summ_zakaz" id="right">{props.post.summa} руб.</div>
-      </div>
-      <div className="post_elem">
-        <div className="comis_zakaz" id="right">{props.post.comis} руб.</div>
+      <div className="post__btns">
+        <button>Изменить</button>
+        <br />
+        <button>Удалить</button>
       </div>
     </div>
-    <div className="post__btns">
-      <button>Изменить</button>
-      <br />
-      <button>Удалить</button>
-    </div>
-  </div>
   );
 };
 
