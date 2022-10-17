875b566e74956812a08da3d46905e5a716a21bfc
APWasilenko <APWasilenko@gmail.com>
2022-10-17 17:00:14 +0700

    17:00 17-10-22

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,15 +3,6 @@ import '../css/Header.css';
 
 
 const Header = () => {
-
-  const [mywidth, setwidth] = useState('');
-  const [myheight, setheigh] = useState('');
-
-  function setStates() {
-    setwidth(window.innerWidth);
-    setheigh(window.innerHeight);
-  }
-
   return (
     <div class="header">
       <div id="left" className="md1"></div>
@@ -21,13 +12,13 @@ const Header = () => {
       <div id="center" className="md5">Автомобиль</div>
       <div class="mydiv1"></div>
       <div id="left" className="md1">Начало</div>
-      <div id="right" className="md2">7:45 06.09.2022</div>
+      <div id="center" className="md2">7:45 06.09.2022</div>
       <div id="right" className="md3">602 457 км</div>
       <div id="right" className="md4">10 789 ₽</div>
       <div id="center" className="md5">889 ₽</div>
       <div class="mydiv1"></div>
       <div id="left" className="md1">Конец</div>
-      <div id="right" className="md2">16:45 06.09.2022</div>
+      <div id="cener" className="md2">16:45 06.09.2022</div>
       <div id="right" className="md3">602 777 км</div>
       <div id="right" className="md4">10 389 ₽</div>
       <div id="center" className="md5">Доход</div>
@@ -38,9 +29,6 @@ const Header = () => {
       <div id="right" className="md4">312 ₽</div>
       <div id="center" className="md5">1 889 ₽</div>
       <div class="mydiv1"></div>
-      <div>Ширина окна - {mywidth}</div>
-      <div>Высота окна - {myheight}</div>
-      <button onClick={setStates}>Вычислить</button>
     </div>
 
   )
