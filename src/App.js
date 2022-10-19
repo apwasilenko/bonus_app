bb1d01456e7ed86b04eda9508e31c13b16364667
Антон Василенко <apwasilenko@gmail.com>
2022-10-19 13:17:39 +0700

    13:15 19-10-22

--- src/App.js
+++ src/App.js
@@ -8,25 +8,25 @@ import './css/App.css';
 
 function App() {
   const [heads] = useState({
-    num: 1, datatimehome: "7:45 06.09.2022", odehome: 602457, balanshome: 10789, auto: 889, datatimeend: "16:45 06.09.2022", odeend: 602777, balansend: 10389, datatime: "10:50", ode: 320, balans: 312, doxod: 1889,
+    key: 1, num: 1, datatimehome: "7:45 06.09.2022", odehome: 602457, balanshome: 10789, auto: 889, datatimeend: "16:45 06.09.2022", odeend: 602777, balansend: 10389, datatime: "10:50", ode: 320, balans: 312, doxod: 1889,
   });
 
   const [posts] = useState([
-    { num: 1, time: '12:23', date: '22.09.2022', distance: '1.2', vremy: '3', summa: '90', comis: '9', form: 'Наличн.', tarif: 'Бонус поселок', avto: '-', zvez: '-', },
-    { num: 11, time: '12:33', date: '22.09.2022', distance: '12.2', vremy: '12', summa: '200', comis: '30', form: 'Безнал.', tarif: 'Бонус деревни', avto: '-', zvez: '3', },
-    { num: 111, time: '12:42', date: '22.09.2022', distance: '124.2', vremy: '134', summa: '2 380', comis: '119', form: 'Сбербанк', tarif: 'Бонус город', avto: '1', zvez: '-', },
-    { num: 1, time: '12:23', date: '22.09.2022', distance: '1.2', vremy: '3', summa: '90', comis: '9', form: 'Наличн.', tarif: 'Бонус поселок', avto: '1', zvez: '3', },
-    { num: 11, time: '12:33', date: '22.09.2022', distance: '12.2', vremy: '12', summa: '200', comis: '30', form: 'Безнал.', tarif: 'Бонус деревни', avto: '1', zvez: '3', },
-    { num: 111, time: '12:42', date: '22.09.2022', distance: '124.2', vremy: '134', summa: '2 380', comis: '119', form: 'Сбербанк', tarif: 'Бонус город', avto: '1', zvez: '3', },
-    { num: 1, time: '12:23', date: '22.09.2022', distance: '1.2', vremy: '3', summa: '90', comis: '9', form: 'Наличн.', tarif: 'Бонус поселок', avto: '1', zvez: '3', },
-    { num: 11, time: '12:33', date: '22.09.2022', distance: '12.2', vremy: '12', summa: '200', comis: '30', form: 'Безнал.', tarif: 'Бонус деревни', avto: '1', zvez: '3', },
-    { num: 111, time: '12:42', date: '22.09.2022', distance: '124.2', vremy: '134', summa: '2 380', comis: '119', form: 'Сбербанк', tarif: 'Бонус город', avto: '1', zvez: '3', },
-    { num: 1, time: '12:23', date: '22.09.2022', distance: '1.2', vremy: '3', summa: '90', comis: '9', form: 'Наличн.', tarif: 'Бонус поселок', avto: '1', zvez: '3', },
-    { num: 11, time: '12:33', date: '22.09.2022', distance: '12.2', vremy: '12', summa: '200', comis: '30', form: 'Безнал.', tarif: 'Бонус деревни', avto: '1', zvez: '3', },
-    { num: 111, time: '12:42', date: '22.09.2022', distance: '124.2', vremy: '134', summa: '2 380', comis: '119', form: 'Сбербанк', tarif: 'Бонус город', avto: '1', zvez: '3', },
-    { num: 1, time: '12:23', date: '22.09.2022', distance: '1.2', vremy: '3', summa: '90', comis: '9', form: 'Наличн.', tarif: 'Бонус поселок', avto: '1', zvez: '3', },
-    { num: 11, time: '12:33', date: '22.09.2022', distance: '12.2', vremy: '12', summa: '200', comis: '30', form: 'Безнал.', tarif: 'Бонус деревни', avto: '1', zvez: '3', },
-    { num: 111, time: '12:42', date: '22.09.2022', distance: '124.2', vremy: '134', summa: '2 380', comis: '119', form: 'Сбербанк', tarif: 'Бонус город', avto: '1', zvez: '3', },
+    { key: 1, num: 1, time: '12:23', date: '22.09.2022', distance: '1.2', vremy: '3', summa: '90', comis: '9', form: 'Наличн.', tarif: 'Бонус поселок', avto: '-', zvez: '-', },
+    { key: 2, num: 11, time: '12:33', date: '22.09.2022', distance: '12.2', vremy: '12', summa: '200', comis: '30', form: 'Безнал.', tarif: 'Бонус деревни', avto: '-', zvez: '3', },
+    { key: 3, num: 111, time: '12:42', date: '22.09.2022', distance: '124.2', vremy: '134', summa: '2 380', comis: '119', form: 'Сбербанк', tarif: 'Бонус город', avto: '1', zvez: '-', },
+    { key: 4, num: 1, time: '12:23', date: '22.09.2022', distance: '1.2', vremy: '3', summa: '90', comis: '9', form: 'Наличн.', tarif: 'Бонус поселок', avto: '1', zvez: '3', },
+    { key: 5, num: 11, time: '12:33', date: '22.09.2022', distance: '12.2', vremy: '12', summa: '200', comis: '30', form: 'Безнал.', tarif: 'Бонус деревни', avto: '1', zvez: '3', },
+    { key: 6, num: 111, time: '12:42', date: '22.09.2022', distance: '124.2', vremy: '134', summa: '2 380', comis: '119', form: 'Сбербанк', tarif: 'Бонус город', avto: '1', zvez: '3', },
+    { key: 7, num: 1, time: '12:23', date: '22.09.2022', distance: '1.2', vremy: '3', summa: '90', comis: '9', form: 'Наличн.', tarif: 'Бонус поселок', avto: '1', zvez: '3', },
+    { key: 8, num: 11, time: '12:33', date: '22.09.2022', distance: '12.2', vremy: '12', summa: '200', comis: '30', form: 'Безнал.', tarif: 'Бонус деревни', avto: '1', zvez: '3', },
+    { key: 9, num: 111, time: '12:42', date: '22.09.2022', distance: '124.2', vremy: '134', summa: '2 380', comis: '119', form: 'Сбербанк', tarif: 'Бонус город', avto: '1', zvez: '3', },
+    { key: 10, num: 1, time: '12:23', date: '22.09.2022', distance: '1.2', vremy: '3', summa: '90', comis: '9', form: 'Наличн.', tarif: 'Бонус поселок', avto: '1', zvez: '3', },
+    { key: 11, num: 11, time: '12:33', date: '22.09.2022', distance: '12.2', vremy: '12', summa: '200', comis: '30', form: 'Безнал.', tarif: 'Бонус деревни', avto: '1', zvez: '3', },
+    { key: 12, num: 111, time: '12:42', date: '22.09.2022', distance: '124.2', vremy: '134', summa: '2 380', comis: '119', form: 'Сбербанк', tarif: 'Бонус город', avto: '1', zvez: '3', },
+    { key: 13, num: 1, time: '12:23', date: '22.09.2022', distance: '1.2', vremy: '3', summa: '90', comis: '9', form: 'Наличн.', tarif: 'Бонус поселок', avto: '1', zvez: '3', },
+    { key: 14, num: 11, time: '12:33', date: '22.09.2022', distance: '12.2', vremy: '12', summa: '200', comis: '30', form: 'Безнал.', tarif: 'Бонус деревни', avto: '1', zvez: '3', },
+    { key: 15, num: 111, time: '12:42', date: '22.09.2022', distance: '124.2', vremy: '134', summa: '2 380', comis: '119', form: 'Сбербанк', tarif: 'Бонус город', avto: '1', zvez: '3', },
   ]);
 
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,31 +5,31 @@ import '../css/Header.css';
 const Header = (props) => {
   console.log(props);
   return (
-    <div class="header">
+    <div className="header">
       <div id="left" className="md1"></div>
       <div id="center" className="md2">Дата время</div>
       <div id="center" className="md3">Одометр</div>
       <div id="center" className="md4">Баланс</div>
       <div id="center" className="md5">Автомобиль</div>
-      <div class="mydiv1"></div>
+      <div className="mydiv1"></div>
       <div id="left" className="md1">Начало</div>
       <div id="center" className="md2">{props.heads.datatimehome}</div>
       <div id="right" className="md3">{props.heads.odehome} км</div>
       <div id="right" className="md4">{props.heads.balanshome} ₽</div>
       <div id="center" className="md5">{props.heads.auto} ₽</div>
-      <div class="mydiv1"></div>
+      <div className="mydiv1"></div>
       <div id="left" className="md1">Конец</div>
       <div id="cener" className="md2">{props.heads.datatimeend}</div>
       <div id="right" className="md3">{props.heads.odeend} км</div>
       <div id="right" className="md4">{props.heads.balansend} ₽</div>
       <div id="center" className="md5">Доход</div>
-      <div class="mydiv1"></div>
+      <div className="mydiv1"></div>
       <div id="left" className="md1">ИТОГО</div>
       <div id="center" className="md2">{props.heads.datatime}</div>
       <div id="right" className="md3">{props.heads.ode} км</div>
       <div id="right" className="md4">{props.heads.balans} ₽</div>
       <div id="center" className="md5">{props.heads.doxod} ₽</div>
-      <div class="mydiv1"></div>
+      <div className="mydiv1"></div>
     </div>
 
   )
